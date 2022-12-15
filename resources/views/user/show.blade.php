ec7ece1b3295a6b7986ae606b5757213c9b6e5fa
Jonathan Isambourg <jonathan.isambourg1@gmail.com>
2022-12-15 09:11:29 +0100

    Intégration view user

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,28 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
-  <!--  <h1>
-        Id : {{$user->id}}
-        Nom : {{$user->name}}
-        Email : {{$user->email}}
-        Avatar : <img src="{{asset('storage/'.$user->avatar)}}">
-    </h1> -->
-
-<!--     <h2>
-        Nombre de likes : {{$nbLikes}}
-        Nombre de commentaires : {{$nbCommentaires}}
-    </h2> -->
+    
 
 <div class="container">
     <div class="profileCard">
         <div class="redcCircle"></div>
-    <div class="avatarProfile"><img class="avatarProfile-img" src="{{asset('storage/images/avatars/avatar-1.png')}}" alt=""></div>
+    <div class="avatarProfile"><img class="avatarProfile-img" src="{{asset('storage/'.$user->avatar)}}" alt=""></div>
     <form id="avatarForm" action="{{ route('user.upload',$user->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="mb-3">
+            <label id="avatarLabel" for="avatar" class="form-label" style="cursor: pointer">Changer d'avatar</label>
             <input class="form-control" type="file" id="avatar" name="avatar" style="display: none">
-            <label id="avatarLabel" for="avatar" class="form-label" style="cursor: pointer"> Changer d'avatar</label>
         </div>
 
         <div class="col-12">
